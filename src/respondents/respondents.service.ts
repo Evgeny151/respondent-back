41a453d0c2a207c79eb72aeae4d5c2fe744469c0
Alex <qistersick@gmail.com>
2025-05-13 22:48:49 +0300

    add types

--- src/respondents/respondents.service.ts
+++ src/respondents/respondents.service.ts
@@ -1,15 +1,42 @@
 // respondents/respondents.service.ts
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { Respondent } from './schemas/respondent.schema'
 
 @Injectable()
 export class RespondentsService {
   constructor(
-    @InjectModel('Respondent') private readonly respondentModel: Model<any>,
+    @InjectModel('Respondent')
+    private readonly respondentModel: Model<Respondent>,
   ) {}
 
   async findAll() {
-    return this.respondentModel.find().exec();
+    return (await this.respondentModel.find().exec()).map((respondent) => {
+      return {
+        _id: respondent._id,
+        gender: respondent.gender,
+        age: respondent.age,
+        city: respondent.city,
+        education: respondent.education,
+        education_two: respondent.education_two,
+        occupation: respondent.occupation,
+        unemployed: respondent.unemployed,
+        period: respondent.period,
+        student: respondent.student,
+        student_job: respondent.student_job,
+        self_employed: respondent.self_employed,
+        employment: respondent.employment,
+        freelance_employment: respondent.freelance_employment,
+        freelance_work: respondent.freelance_work,
+        field_work: respondent.field_work,
+        income: respondent.income,
+        job_device: respondent.job_device,
+        leisure_device: respondent.leisure_device,
+        research: respondent.research,
+        platform: respondent.platform,
+        time: respondent.time,
+      }
+    })
   }
 }
--- src/respondents/schemas/respondent.schema.ts
+++ src/respondents/schemas/respondent.schema.ts
@@ -1,11 +1,97 @@
-import { Schema } from 'mongoose';
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
 
-export const RespondentSchema = new Schema({
-  age: String,
-  income_level: String,
-  gender: String,
-  answers: {
-    type: Map,
-    of: String,
-  },
-});
+@Schema()
+export class Respondent {
+  @Prop()
+  to_send: string;
+
+  @Prop()
+  user_id: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  gender: string;
+  @Prop()
+  name: string;
+  @Prop()
+  age: string;
+  @Prop()
+  city: string;
+  @Prop()
+  education: string;
+  @Prop()
+  education_two: string;
+  @Prop()
+  occupation: string;
+  @Prop()
+  unemployed: string;
+  @Prop()
+  period: string;
+  @Prop()
+  student: string;
+  @Prop()
+  student_job: string;
+  @Prop()
+  self_employed: string;
+  @Prop()
+  employment: string;
+  @Prop()
+  freelance_employment: string;
+  @Prop()
+  freelance_work: string;
+  @Prop()
+  field_work: string;
+  @Prop()
+  income: string;
+  @Prop()
+  job_device: string;
+  @Prop()
+  leisure_device: string;
+  @Prop()
+  research: string;
+  @Prop()
+  platform: string;
+  @Prop()
+  time: string;
+  @Prop()
+  privacy: string;
+  @Prop({ type: Map, of: String })
+  answers: Map<string, string>;
+}
+
+export const RespondentSchema = SchemaFactory.createForClass(Respondent);
+
+// export const RespondentSchema = new Schema({
+//   to_send: String,
+//   user_id: String,
+//   username: String,
+//   gender: String,
+//   name: String,
+//   age: String,
+//   city: String,
+//   education: String,
+//   education_two: String,
+//   occupation: String,
+//   unemployed: String,
+//   period: String,
+//   student: String,
+//   student_job: String,
+//   self_employed: String,
+//   employment: String,
+//   freelance_employment: String,
+//   freelance_work: String,
+//   field_work: String,
+//   income: String,
+//   job_device: String,
+//   leisure_device: String,
+//   research: String,
+//   platform: String,
+//   time: String,
+//   privacy: String,
+//   answers: {
+//     type: Map,
+//     of: String,
+//   },
+// });
